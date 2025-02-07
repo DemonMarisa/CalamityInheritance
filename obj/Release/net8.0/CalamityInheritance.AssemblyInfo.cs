1de234b979d3d6c01a2186e053b9083891e0c36e
DemonMarisa <121868896@qq.com>
2025-02-07 18:17:31 +0800

    版本和计划更新

--- obj/Release/net8.0/CalamityInheritance.AssemblyInfo.cs
+++ obj/Release/net8.0/CalamityInheritance.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CalamityInheritance")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+382e0ff8c46430faf7d66dd1421b9f82810f2561")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c353b7e86a8e59ca0ceb0881f2dfa75f08fce38e")]
 [assembly: System.Reflection.AssemblyProductAttribute("CalamityInheritance")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CalamityInheritance")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
